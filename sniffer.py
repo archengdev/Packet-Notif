f5d47bd6d694d2b3d1bd5aae97dd1f0e4fd40c0f
archengdev <arc.albertcheng@gmail.com>
2024-07-30 12:25:02 +0800

    removed redundant functions

--- sniffer.py
+++ sniffer.py
@@ -6,14 +6,9 @@ import socket
 from arc4 import ARC4
 
 import threading
-
 import chime
 
- 
-
-
-chime.info()
-
+# initialize variables for packet decryption
 conf.sniff_promisc = 0 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.connect(('8.8.8.8', 80))
@@ -26,7 +21,8 @@ predicted_seq_server = -1
 ooo_storage = {}
 history = {}
 decryption_history = b''
- 
+
+# function to parse packets
 def parse_stream(packet):
 	global predicted_seq_server
 	global ooo_storage
@@ -51,7 +47,7 @@ def parse_stream(packet):
 					predicted_seq_server = packet[TCP].seq + len(packet_data)
 					packet_buffer = history[packet[TCP].seq][0]
 					arc4_server = ARC4(KEY_INCOMING)
-					server_decrypt_arc4(history[packet[TCP].seq][1])
+					arc4_server.decrypt(history[packet[TCP].seq][1])
 					decryption_history = history[packet[TCP].seq][1]
 					identify_packets(packet_data)
  
@@ -63,7 +59,6 @@ def parse_stream(packet):
 							del ooo_storage[predicted_seq_server]
 							predicted_seq_server += len(target_load)
 							identify_packets(target_load)
- 
  
 def identify_packets(data=b''): 
 	global packet_buffer
@@ -78,16 +73,10 @@ def identify_packets(data=b''):
  
 def decrypt_packet(packet_data):
 	global decryption_history
-	decrypted = server_decrypt_arc4(packet_data[5:])
-		# decrypted = packet_data[0:5]+server_decrypt_arc4(packet_data[5:])
+	decrypted = arc4_server.decrypt(packet_data[5:])
 	decryption_history += packet_data[5:]
-	packet_id = packet_data[4]
-	# if packet_id not in (92, 8, 10, 42, 11, 44, 120, 6, 45, 117, 12): print(packet_id)
-	# either 101 or 114
-	handler = HANDLERS.get(packet_id)
-	if handler:
-		# print(packet_id)
-		handler(decrypted)
+	if packet_data[4] == 67:
+		handle_notification(decrypted)
  
 def hello_reset(packet):
 	global packet_buffer
@@ -103,30 +92,18 @@ def hello_reset(packet):
 	ooo_storage = {}
 	decryption_history = b''
 	history = {}
- 
- 
-def server_decrypt_arc4(ciphertext):
-	return arc4_server.decrypt(ciphertext)
- 
-def handle_chat(decrypted):
-	print(str(decrypted)) 
-    # pass
 
- 
+NOTIFS = {92:'WLAB', 93:'DD', 46:'CD', 162:'ANEST'}
 def handle_notification(decrypted):
-	print(type(decrypted))
 	string = str(decrypted)
-	print(string)
-	print(decrypted.decode('utf_8'))
-	# print(string)
-	if string[21:22] == 'd':
-		print(string)
-		chime.info()
-		# tab_in()
+
+	if string[21] == 'd':
+		identif = decrypted[-1]
+		if identif in NOTIFS:
+			chime.success()
+			print(NOTIFS[identif])
+		else: print(string); print(decrypted[-1]); chime.info()
 		
-    
-HANDLERS = {67:handle_notification}
- 
 def main():
 	sniff(filter = 'tcp port 2050', iface = 'WiFi',prn = parse_stream)
  
@@ -135,26 +112,13 @@ x.start()
 
 
 
-"""
-misc notes:
-
-hello: 92
-new dungeon: 101/114??????????
-move: 42
-chat: 44
-quests: 6
-random:12/18/139
-
-random stuff: 8/10
-more randomstuff: 11/67/ 120/45/117
-"""
-
 # moonline: b'\x08\n\x008{"k":"s.dungeon_opened_by","t":{"player":"Bobbybeast",}}\x00\x00O\xdf'
 
 # shaitain: b'\x08\n\x006{"k":"s.dungeon_opened_by","t":{"player":"BIGHURTS",}}\x00\x00m\x99'
 
 #ot: b'\x08\n\x006{"k":"s.dungeon_opened_by","t":{"player":"Coolfood",}}\x00\x00\x070'
 #ot: b'\x08\n\x008{"k":"s.dungeon_opened_by","t":{"player":"Sleepersun",}}\x00\x00\x070'
+# 48
 
 #mv: b'\x08\n\x006{"k":"s.dungeon_opened_by","t":{"player":"Huntrexx",}}\x00\x00O\xdf'
 
