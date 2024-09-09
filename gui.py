47f6a49e3244cc094f5f27b3c1b87ca2315052fa
archengdev <arc.albertcheng@gmail.com>
2024-09-09 16:20:44 +0800

    using canvas for image transparency

--- gui.py
+++ gui.py
@@ -4,32 +4,59 @@ from PIL import ImageTk, Image
 import customtkinter as ctk
 import os
 
-# fix some pngs (3d)
-
-# make dungeons grayscale?
-# click and hold to choose?
-# choose from recently popped?
-
+dung_dir = './dungeons/'
+dung_files = os.listdir(dung_dir)
+print(len(dung_files))
 
 root = ctk.CTk()
 root.title("Packet Sniffer")
-root.geometry("500x300")
+root.geometry("550x500")
 root.iconbitmap('./logo.ico')
 
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1,weight=1)
+root.columnconfigure(2,weight=1)
+root.columnconfigure(3,weight=1)
+root.rowconfigure(1, weight=1)
 
-# frame =customtkinter.CTkScrollableFrame(root)
-# # frame =customtkinter.CTkScrollableFrame(root, orientation="horizontal")
-# frame.grid(row=0, column=0, sticky="nsew")
+# for the title
+title = ctk.CTkLabel(root,
+                     text="RotMG Packet Sniffer",
+                     font=("helvetica",13))
+title.grid(row=0,column=1,columnspan=2,pady=5)
+
+# , label_text="Select which dungeons you want to be notified for"
+# for the list of dungeons to select
+frame = ctk.CTkCanvas(root, bg='black')
+frame.grid(row=1,column=0,columnspan=4,sticky="nsew",pady=5,padx=5)
+
+
+
+photoimage = ImageTk.PhotoImage(file="./dungeons/udl.png")
+
+
+# start button
+start = ctk.CTkButton(root, image=photoimage)
+start.grid(row=2,column=1)
+
+# stop button
+stop = ctk.CTkButton(root)
+stop.grid(row=2,column=2)
+
+# # dungeon history
+hist = ctk.CTkScrollableFrame(root,orientation="horizontal",height=37,
+                              label_text="Recently popped dungeons")
+
+# label_text_color
+# label_fg_color
+# label_font
+
+hist._scrollbar.configure(height=12)
+hist.grid(row=3,column=0,columnspan=4,sticky="nsew",pady=5,padx=5)
+
 
 # for x in range(20):
 #     customtkinter.CTkButton(frame, text="button").pack(pady=10)
-
-# root.columnconfigure(0, weight=1)
-# root.rowconfigure(0, weight=1)
-# root.rowconfigure(1,weight=0)
-# frame.columnconfigure(0, weight=1)
-# frame.rowconfigure(1, weight=1)
-
 
 # file = './dungeons/3d.png'
 
@@ -41,14 +68,11 @@ root.iconbitmap('./logo.ico')
 # label.image = img
 # label.pack()
 # aby = ImageTk.PhotoImage(Image.open("./dungeons/3d.png"))
-# abyl = tk.Label(root, image=aby)
+# abyl = tk.Label(frame, image=aby)
+# abyl.pack(side='left')
 # udl = ImageTk.PhotoImage(Image.open("./dungeons/udl.png"))
 # udll = tk.Label(root,image=udl)
 
-# abyl.grid(row=0,column=0)
-# udll.grid(row=0,column=1)
-# udll.pack(side = "top", fill = "none")
-# abyl.pack(side = "bottom", fill = "both", expand="yes")
 
 # forax = tk.PhotoImage(file=r"./dungeons/forax.png")
 # threed = tk.PhotoImage(file=r"./dungeons/snake.png")
@@ -57,10 +81,41 @@ root.iconbitmap('./logo.ico')
 # tk.Button(root,image=t2).pack(side="top")
 # tk.Button(root, image=test).pack(side="top")
 
-#--------
-# root.columnconfigure(0, weight=1)
-# root.columnconfigure(1,weight=2)
-# root.rowconfigure(0, weight=1)
+# imgs =[]
+# labels=[]
+# for i, file in enumerate(dung_files):
+#     img = Image.open(dung_dir + file).resize((30, 30))
+#     print(img.mode)
+#     img = ImageTk.PhotoImage(Image.open(dung_dir + file).resize((30, 30)))
+    
+#     imgs.append(img)
+#     # img.configure()
+#     l1 = tk.Label(frame, image=imgs[i])
+#     labels.append(l1)
+
+
+# for label in labels:
+#     label.pack(side='left', anchor='nw')
+
+
+# frame.create_image(150,150,image=photoimage)
+imgs = []
+for file in dung_files:
+    im = Image.open(dung_dir + file).resize((30,30))
+    image = ImageTk.PhotoImage(im)
+    imgs.append(image)
+
+for i in range(60):
+    frame.create_image(0+i*10,0+i*10,image=imgs[i])
+
+# i2 = ImageTk.PhotoImage(Image.open(dung_dir + 'aby.png').resize((30, 30)))
+# # img.configure()
+# l2 = tk.Label(frame, image=i2)
+# # label.image = img # this feels redundant but the image didn't show up without it in my app
+# l2.pack(side='left', anchor='nw')
+
+
+root.mainloop()
 
 
 # def add_to_list(event=None):
@@ -70,65 +125,4 @@ root.iconbitmap('./logo.ico')
 #         entry.delete(0, tk.END)
 
 
-# frame = ttk.Frame(root)
-# frame.grid(row=0,column=0, sticky="nsew", padx=5, pady=5)
-
-# frame.columnconfigure(0, weight=1)
-# frame.rowconfigure(1, weight=1)
-
-# entry = ttk.Entry(frame)
-# entry.grid(row=0, column=0, sticky="ew")
-
-# entry.bind("<Return>", add_to_list)
-# # will pass event object to the function, can use lambda function
-
 # entry_btn = ttk.Button(frame, text="add", command = add_to_list)
-# entry_btn.grid(row=0, column=1)
-
-# text_list = tk.Listbox(frame)
-# text_list.grid(row=1,column=0, columnspan=2, sticky="nsew")
-#--------
-
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1,weight=1)
-root.columnconfigure(2,weight=1)
-root.columnconfigure(3,weight=1)
-root.rowconfigure(1, weight=1)
-
-# for the title
-title = ctk.CTkLabel(root,
-                     text="RotMG Packet Sniffer: Choose which dungeons you want to be notified for",
-                     font=("helvetica",13))
-title.grid(row=0,column=1,columnspan=2,pady=5)
-
-# for the list of dungeons to select
-frame = ctk.CTkScrollableFrame(root)
-frame.grid(row=1,column=0,columnspan=4,sticky="nsew",pady=5,padx=5)
-
-# start button
-start = ctk.CTkButton(root)
-start.grid(row=2,column=1)
-
-# stop button
-stop = ctk.CTkButton(root)
-stop.grid(row=2,column=2)
-
-# # dungeon history
-hist = ctk.CTkScrollableFrame(root,orientation="horizontal",height=37,
-                              label_text="hello")
-
-# label_text_color
-# label_fg_color
-# label_font
-
-hist._scrollbar.configure(height=12)
-hist.grid(row=3,column=0,columnspan=4,sticky="nsew",pady=5,padx=5)
-
-a = ttk.Label(hist,text="test")
-a.grid(row=10,column=1)
-b = ttk.Label(frame,text="test")
-b.grid(row=10,column=0)
-
-
-
-root.mainloop()
